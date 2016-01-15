aa6c05dbcdb1612271d3f7c3e1ef1af3cb8b3a49
lenin <lenin.yepez@gmail.com>
2016-01-15 14:16:12 -0500

    taller

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+	<link type="text/css" href="css/main.css"/>
+</head>
+<body>
+<header>
+</header>
+<section>
+	id=main
+</section>
+
+</body>
+</html>
